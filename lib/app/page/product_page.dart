3f85fb5b7e1ce6cc0f1eea0e45d6139fd876990d
MrBigPorter <mrsuperporter@gmail.com>
2025-10-10 20:13:07 +0800

    fix: remove unused import for flutter foundation in product page

--- lib/app/page/product_page.dart
+++ lib/app/page/product_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/common.dart';
 import 'package:flutter_app/components/base_scaffold.dart';
